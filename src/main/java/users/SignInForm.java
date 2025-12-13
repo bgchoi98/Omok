56576823b2b5c19c5aa89311018bf63084ce7531
rbxo3213 <rbxo3213@gmail.com>
2025-12-13 10:10:09 +0900

    signInForm class gen

---
+++ src/main/java/users/SignInForm.java
@@ -0,0 +1,20 @@
+package users;
+
+public class SignInForm {
+	private String userId;
+	private String userPw;
+	
+	public SignInForm(String userId, String userPw) {
+		this.userId = userId;
+		this.userPw = userPw;
+		
+	}
+	
+	public boolean isValid(String userId, String userPw) {
+		boolean result = false;
+		
+		
+		
+		return result;
+	}
+}
--- src/main/java/users/usersController/UsersServlet.java
+++ src/main/java/users/usersController/UsersServlet.java
@@ -1,6 +1,7 @@
 package users.usersController;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import users.SignInForm;
 import users.User;
 import users.usersService.UserService;
 
@@ -54,18 +56,27 @@ public class UsersServlet extends HttpServlet {
 			// 로그인 기능 구현
 			String id = req.getParameter("user_id");
 			String pw = req.getParameter("user_pw");
-			User user = USERSERVICE.login(id, pw);
+			SignInForm signInForm = new SignInForm(id, pw);
+
+			User user = null;
 			
-			if (user == null) {
-				req.setAttribute("errorMessage", "아이디 또는 비밀번호가 올바르지 않습니다.");
-				req.getRequestDispatcher("/signIn.jsp").forward(req, res);
-				return;
+			if (signInForm.isValid(id, pw)) {
+				user = USERSERVICE.login(id, pw); // 사용자 form이 정상이면
+				if (user == null) { // DB에서 불러온 user가 null이면 다시 SignIn 페이지로 보냄
+					req.setAttribute("errorMessage", "아이디 또는 비밀번호가 올바르지 않습니다.");
+					req.getRequestDispatcher("/signIn.jsp").forward(req, res);
+					return;
+				} else { // DB에서 불러온 user가 정상이면 메인 화면으로 보냄
+				    HttpSession session = req.getSession();	//세션 저장
+				    session.setAttribute("user_id", id);
+				    //상원님이 말한 prg 패턴인듯 리다이렉트를해야 get요청으로 이동해서 post요청이 안나는듯?
+				    res.sendRedirect("/Omok/main.jsp");
+				}				
 			} else {
-			    HttpSession session = req.getSession();	//세션 저장
-			    session.setAttribute("user_id", id);
-			    //상원님이 말한 prg 패턴인듯 리다이렉트를해야 get요청으로 이동해서 post요청이 안나는듯?
-			    res.sendRedirect("/Omok/main.jsp");
+				req.getRequestDispatcher("/SignIn.jsp").forward(req, res);
 			}
+			
+
 		} else if (uri.equals("/sign/signWithdraw")) {
 			// 회원 탈퇴 기능 구현
 			
